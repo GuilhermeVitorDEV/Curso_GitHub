5f9fc0618cf7ad83b0f6f193e26aa13dd51690cc
guilherme.vitor <guilherme.vitormeca@gmail.com>
2025-02-07 09:52:04 -0300

    Clean Dataset

--- clean_dataset.py
+++ clean_dataset.py
@@ -6,4 +6,12 @@ df = pandas.DataFrame(file)
 
 df.rename(columns={"Date":"Date","Daily total female births in California, 1959":"Births(1959) in California"})
 
-print(df.head())
+missing_data = df.isnull().sum()
+
+total_cells = numpy.prod(df.shape)
+total_missing = missing_data.sum()
+
+# percent of data that is missing
+percent_missing = (total_missing/total_cells) * 100
+
+df.dropna()
